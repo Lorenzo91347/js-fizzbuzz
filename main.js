ab898d51549859c08d5f7c596e3230dd54b12f27
Lorenzo91347 <lorenzo.losito91@gmail.com>
2023-11-08 16:54:44 +0100

    feat:added li creation and population

--- main.js
+++ main.js
@@ -5,15 +5,25 @@ let startCounter = 1;
 let endCounter = 100;
 
 for (let counter = startCounter; counter <= endCounter; counter++) {
+    const li = document.createElement('li');
+
     if (counter % 3 === 0 && counter % 5 === 0) {
         console.log("FizzBuzz");
+        li.append("Fizzbuzz");
 
     } else if (counter % 3 === 0) {
         console.log("Buzz");
+        li.append("Buzz")
 
     } else if (counter % 5 === 0) {
         console.log("Fizz");
+        li.append("Fizz")
+
+    } else {
+        console.log(counter);
+        li.append(counter)
     }
-    else (console.log(counter))
+
+    ul.append(li);
 
 }
